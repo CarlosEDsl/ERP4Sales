44ea909f810dbe120bd83476a797f94900464a75
CarlosEDsl <carloseduardoestds@gmail.com>
2024-05-21 18:19:28 -0300

    First ERP creation

---
+++ src/main/java/com/carloseduardo/ERP4Sales/Erp4SalesApplication.java
@@ -0,0 +1,13 @@
+package com.carloseduardo.ERP4Sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Erp4SalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Erp4SalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carloseduardo/ERP4Sales/Erp4SalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carloseduardo.ERP4Sales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Erp4SalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
