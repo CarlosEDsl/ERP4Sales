635a97100dc4d81e259a4d1471fda830683026a8
CarlosEDsl <carloseduardoestds@gmail.com>
2024-05-22 15:21:35 -0300

    Category model creation

--- src/main/java/com/eduardocarlos/ERP4Sales/model/domain/Category.java
+++ src/main/java/com/eduardocarlos/ERP4Sales/model/domain/Category.java
@@ -1,2 +1,30 @@
-package com.eduardocarlos.ERP4Sales.model.domain;public class Category {
+package com.eduardocarlos.ERP4Sales.model.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = Category.TABLE_NAME)
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Category {
+    public static final String TABLE_NAME = "category";
+
+    @Id
+    @Column(name = "id", unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", unique = true, nullable = false)
+    @NotBlank
+    private String name;
+
+    @Column(name = "description", length = 250)
+    private String description;
+
 }
