188cbf295ee104da35ccb250bb5e5660786bdea9
CarlosEDsl <carloseduardoestds@gmail.com>
2024-06-03 17:38:22 -0300

    ObjectNotFoundException implementation

--- src/main/java/com/eduardocarlos/ERP4Sales/sevices/exceptions/ObjectNotFoundException.java
+++ src/main/java/com/eduardocarlos/ERP4Sales/sevices/exceptions/ObjectNotFoundException.java
@@ -1,2 +1,15 @@
-package com.eduardocarlos.ERP4Sales.sevices.exceptions;public class ObjectNotFoundException {
+package com.eduardocarlos.ERP4Sales.sevices.exceptions;
+
+import jakarta.persistence.EntityNotFoundException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ObjectNotFoundException extends EntityNotFoundException {
+
+    public ObjectNotFoundException(String message){
+        super(message);
+    }
+
 }
